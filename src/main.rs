b8140c6d19ead1274117b63f356f50ce92bd1632
Night_Hunter <samuelhuntnz@gmail.com>
2022-12-10 03:25:38 +1300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,182 @@
+#![warn(clippy::str_to_string)]
+
+mod commands;
+use commands::*;
+
+use dotenv::dotenv;
+use poise::serenity_prelude as serenity;
+use std::{collections::HashMap, env::var, sync::Mutex, time::Duration};
+
+// Types used by all command functions
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[derive(Debug, Clone)]
+pub struct Poll {
+    description: String,
+    options: Vec<(String, u32)>,
+}
+
+// Custom user data passed to all command functions
+pub struct Data {
+    votes: Mutex<HashMap<String, u32>>,
+    poll: Mutex<HashMap<String, Poll>>,
+}
+
+/// Show this help menu
+#[poise::command(prefix_command, track_edits, slash_command)]
+async fn help(
+    ctx: Context<'_>,
+    #[description = "Specific command to show help about"]
+    #[autocomplete = "poise::builtins::autocomplete_command"]
+    command: Option<String>,
+) -> Result<(), Error> {
+    poise::builtins::help(
+        ctx,
+        command.as_deref(),
+        poise::builtins::HelpConfiguration {
+            extra_text_at_bottom: "\
+This is an example bot made to showcase features of my custom Discord bot framework",
+            show_context_menu_commands: true,
+            ..Default::default()
+        },
+    )
+    .await?;
+    Ok(())
+}
+
+/// Registers or unregisters application commands in this guild or globally
+#[poise::command(prefix_command, hide_in_help)]
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+
+    Ok(())
+}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().unwrap();
+    env_logger::init();
+
+    let options = poise::FrameworkOptions {
+        commands: vec![
+            help(),
+            register(),
+            general::vote(),
+            general::getPoll(),
+            general::createPoll(),
+            general::endPoll(),
+            general::addmultiple(),
+            general::choice(),
+            general::boop(),
+            general::voiceinfo(),
+            general::test_reuse_response(),
+            general::oracle(),
+            general::code(),
+            general::say(),
+            general::file_details(),
+            general::totalsize(),
+            general::modal(),
+            general::punish(),
+            #[cfg(feature = "cache")]
+            general::servers(),
+            general::reply(),
+            general::add(),
+            context_menu::user_info(),
+            context_menu::echo(),
+            autocomplete::greet(),
+            checks::shutdown(),
+            checks::modonly(),
+            checks::delete(),
+            checks::ferrisparty(),
+            checks::cooldowns(),
+            checks::minmax(),
+            checks::get_guild_name(),
+            checks::only_in_dms(),
+            checks::lennyface(),
+            checks::permissions_v2(),
+            subcommands::parent(),
+            localization::welcome(),
+        ],
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("~".into()),
+            edit_tracker: Some(poise::EditTracker::for_timespan(Duration::from_secs(3600))),
+            additional_prefixes: vec![
+                poise::Prefix::Literal("hey bot"),
+                poise::Prefix::Literal("hey bot,"),
+            ],
+            ..Default::default()
+        },
+        /// The global error handler for all error cases that may occur
+        on_error: |error| Box::pin(on_error(error)),
+        /// This code is run before every command
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("Executing command {}...", ctx.command().qualified_name);
+            })
+        },
+        /// This code is run after a command if it was successful (returned Ok)
+        post_command: |ctx| {
+            Box::pin(async move {
+                println!("Executed command {}!", ctx.command().qualified_name);
+            })
+        },
+        /// Every command invocation must pass this check to continue execution
+        command_check: Some(|ctx| {
+            Box::pin(async move {
+                if ctx.author().id == 123456789 {
+                    return Ok(false);
+                }
+                Ok(true)
+            })
+        }),
+        /// Enforce command checks even for owners (enforced by default)
+        /// Set to true to bypass checks, which is useful for testing
+        skip_checks_for_owners: false,
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(async move {
+                println!("Got an event in event handler: {:?}", event.name());
+                Ok(())
+            })
+        },
+        ..Default::default()
+    };
+
+    poise::Framework::builder()
+        .token(
+            var("TOKEN")
+                .expect("Missing `DISCORD_TOKEN` env var, see README for more information."),
+        )
+        .setup(move |_ctx, _ready, _framework| {
+            Box::pin(async move {
+                Ok(Data {
+                    votes: Mutex::new(HashMap::new()),
+                    poll: Mutex::new(HashMap::new()),
+                })
+            })
+        })
+        .options(options)
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        )
+        .run()
+        .await
+        .unwrap();
+}
